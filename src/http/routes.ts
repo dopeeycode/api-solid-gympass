cd424143b6b70401c88178847e375512f6269283
Italo <italodopeey@gmail.com>
2023-11-01 18:22:21 -0300

    feat: create session authenticate

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
+import { authenticateUserController } from './controllers/authenticate'
 import { registerUserController } from './controllers/register'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', registerUserController)
+  app.post('/sessions', authenticateUserController)
 }
