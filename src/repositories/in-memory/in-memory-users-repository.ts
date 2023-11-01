c6d6ae978665835291f614a8226cc19299859781
Italo <italodopeey@gmail.com>
2023-11-01 14:30:39 -0300

    feat: using in memory repository

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,31 @@
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public db: User[] = []
+
+  async findByEmail(email: string) {
+    const user = this.db.find((user) => user.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const { name, email, password_hash } = data
+    const user = {
+      id: 'user-1-test',
+      name,
+      email,
+      password_hash,
+      created_at: new Date(),
+    }
+    this.db.push(user)
+    console.log(this.db)
+
+    return user
+  }
+}
